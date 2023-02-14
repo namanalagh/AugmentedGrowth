c5ecb6f7b2896441255162ded23e6a8dfbb45143
Naman Alagh <namanalagh19@gmail.com>
2023-02-14 22:07:03 +0530

    .

--- AugmentedGrowth/TopicListTableViewController.swift
+++ AugmentedGrowth/TopicListTableViewController.swift
@@ -9,9 +9,38 @@ import UIKit
 
 class TopicListTableViewController: UITableViewController {
 
+    var subjectTableViewController: SubjectTableViewController?
+    
+    var selectedTopic: String!
+    
+    var classes: [Grade] = [
+        Grade(gradeNo: 1, title: "Class 1"),
+        Grade(gradeNo: 2, title: "Class 2"),
+        Grade(gradeNo: 3, title: "Class 3"),
+        Grade(gradeNo: 4, title: "Class 4"),
+        Grade(gradeNo: 5, title: "Class 5"),
+        Grade(gradeNo: 6, title: "Class 6"),
+        Grade(gradeNo: 7, title: "Class 7"),
+        Grade(gradeNo: 8, title: "Class 8"),
+        Grade(gradeNo: 9, title: "Class 9"),
+        Grade(gradeNo: 10, title: "Class 10")
+    ]
+    
+    var subjects: [Subject] = [
+        Subject(title: "Mathematics"),
+        Subject(title: "Chemistry"),
+        Subject(title: "Biology"),
+        Subject(title: "Astronomy"),
+        Subject(title: "Physics")
+    ]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Topics"
+        
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "ClassCell")
+
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -21,25 +50,39 @@ class TopicListTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "subjectTableView" {
+            let sharedTopic = selectedTopic
+            let destinationVC = segue.destination as! SubjectTableViewController
+            destinationVC.selectedTopic = sharedTopic!
+        }
+    }
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return subjects.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ClassCell", for: indexPath)
+        let sub = subjects[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        content.text = "\(sub.title)"
+        cell.contentConfiguration = content
         return cell
     }
-    */
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let sub = subjects[indexPath.row]
+        selectedTopic = sub.title
+        self.performSegue(withIdentifier: "subjectTableView", sender: self)
+
+    }
 
     /*
     // Override to support conditional editing of the table view.
--- Topic.swift
+++ Topic.swift
@@ -12,8 +12,46 @@ struct Topic {
     var subject: String
     var topicNo: Int
     var topicTitle: String
+    var difficulty: String
+    var image: String?
 }
 
 struct Grade {
-    var number: Int
+    var gradeNo: Int
+    var title: String
 }
+
+struct Subject {
+    var title: String
+}
+
+var allTopics: [Topic] = [
+    Topic(grade: 1, subject: "Mathematics", topicNo: 1, topicTitle: "Shapes and Spatial Understanding", difficulty: "Easy"),
+    Topic(grade: 1, subject: "Mathematics", topicNo: 2, topicTitle: "Solids Around Us", difficulty: "Easy"),
+    Topic(grade: 1, subject: "Mathematics", topicNo: 3, topicTitle: "Numbers from 0-9", difficulty: "Easy"),
+    Topic(grade: 1, subject: "Mathematics", topicNo: 4, topicTitle: "Numbers from 10-20", difficulty: "Easy"),
+    Topic(grade: 1, subject: "Mathematics", topicNo: 5, topicTitle: "Addition And Subtraction (Upto 20)", difficulty: "Easy"),
+    Topic(grade: 1, subject: "Mathematics", topicNo: 6, topicTitle: "Numbers from 21–99", difficulty: "Easy"),
+    Topic(grade: 1, subject: "Mathematics", topicNo: 7, topicTitle: "Mental Arithmetic", difficulty: "Easy"),
+    Topic(grade: 1, subject: "Mathematics", topicNo: 8, topicTitle: "Money", difficulty: "Easy"),
+    Topic(grade: 1, subject: "Mathematics", topicNo: 9, topicTitle: "Measurements", difficulty: "Easy"),
+    Topic(grade: 1, subject: "Mathematics", topicNo: 10, topicTitle: "Patterns", difficulty: "Easy"),
+    Topic(grade: 8, subject: "Chemistry", topicNo: 1, topicTitle: "Matter", difficulty: "Easy"),
+    Topic(grade: 8, subject: "Chemistry", topicNo: 2, topicTitle: "Physical and Chemical Changes", difficulty: "Easy"),
+    Topic(grade: 8, subject: "Chemistry", topicNo: 3, topicTitle: "Elements, Compounds and Mixtures", difficulty: "Easy"),
+    Topic(grade: 8, subject: "Chemistry", topicNo: 4, topicTitle: "Atomic Structure", difficulty: "Easy"),
+    Topic(grade: 8, subject: "Chemistry", topicNo: 5, topicTitle: "Language of Chemistry", difficulty: "Easy"),
+    Topic(grade: 8, subject: "Chemistry", topicNo: 6, topicTitle: "Chemical Reactions", difficulty: "Easy"),
+    Topic(grade: 8, subject: "Chemistry", topicNo: 7, topicTitle: "Hydrogen", difficulty: "Easy"),
+    Topic(grade: 8, subject: "Chemistry", topicNo: 8, topicTitle: "Water", difficulty: "Easy"),
+    Topic(grade: 8, subject: "Chemistry", topicNo: 9, topicTitle: "Carbon and its Compounds", difficulty: "Easy"),
+    Topic(grade: 8, subject: "Chemistry", topicNo: 10, topicTitle: "Water", difficulty: "Easy"),
+    Topic(grade: 8, subject: "Astronomy", topicNo: 1, topicTitle: "Journey to Outer Space", difficulty: "Easy"),
+    Topic(grade: 8, subject: "Astronomy", topicNo: 2, topicTitle: "Explore Jupiter", difficulty: "Easy"),
+    Topic(grade: 8, subject: "Astronomy", topicNo: 3, topicTitle: "The Asteroid Belt", difficulty: "Easy"),
+    Topic(grade: 8, subject: "Astronomy", topicNo: 4, topicTitle: "Saturn's Rings", difficulty: "Easy"),
+    Topic(grade: 8, subject: "Astronomy", topicNo: 5, topicTitle: "Explore Venus", difficulty: "Easy"),
+    Topic(grade: 8, subject: "Astronomy", topicNo: 6, topicTitle: "Explore Mars", difficulty: "Easy"),
+    Topic(grade: 8, subject: "Astronomy", topicNo: 7, topicTitle: "Earth: Our Home Planet", difficulty: "Easy"),
+    
+]
