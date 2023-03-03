7cb5b1bcde46f23c5296a8ca2f47ac787c3c6886
Naman Alagh <namanalagh19@gmail.com>
2023-03-03 10:24:02 +0530

    .

--- GeographyViewController.swift
+++ GeographyViewController.swift
@@ -9,21 +9,25 @@ import UIKit
 
 class GeographyViewController: UIViewController {
 
+   
+    @IBOutlet weak var scrollViewOutlet: UIScrollView!
+    
+    @IBOutlet weak var subviewOutlet: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        scrollViewOutlet.delegate = self
+        scrollViewOutlet.maximumZoomScale = 2
     }
     
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+
+extension GeographyViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return subviewOutlet
     }
-    */
-
 }
